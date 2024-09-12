1446b0db50ce8e4aa213ed4ebe4319c46c634df4
Benjamin Pang (molarmanful) <benjinpang@gmail.com>
2024-09-11 22:27:27 -0400

    base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,5 @@
+import process from 'node:process'
+
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
@@ -5,6 +7,9 @@ const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
+  },
+  paths: {
+    base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
   },
 }
 
