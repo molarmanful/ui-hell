32a2ebe47c3599a30c9017578238d1e1310c2c95
Benjamin Pang (molarmanful) <benjinpang@gmail.com>
2024-09-11 22:44:56 -0400

    Revert 1 commits
    1446b0d 'base path'

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,3 @@
-import process from 'node:process'
-
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
@@ -7,9 +5,6 @@ const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
-  },
-  paths: {
-    base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
   },
 }
 
